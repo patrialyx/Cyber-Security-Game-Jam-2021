abeb26a112eca25d9aa17e011709a90b250252d7
Patria Lim <patria.lyx@gmail.com>
2021-06-29 00:06:01 +0800

    restricted player movement so that it wont move out of the playing space. adjusted camera movement to only show game space.

--- Cyber Security Game Jam 2021/Assets/Scripts/MainCharacterMovement.cs
+++ Cyber Security Game Jam 2021/Assets/Scripts/MainCharacterMovement.cs
@@ -8,6 +8,15 @@ public class MainCharacterMovement : MonoBehaviour
     public float JumpForce = 10;
     private Rigidbody2D _rigidbody;
 
+    [SerializeField]
+    private float leftLimit;
+    [SerializeField]
+    private float rightLimit;
+    [SerializeField]
+    private float bottomLimit; 
+    [SerializeField]
+    private float topLimit; 
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -16,11 +25,28 @@ public class MainCharacterMovement : MonoBehaviour
     private void Update() 
     {
         var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        Debug.Log("movement"+ movement.ToString());
+        Debug.Log("leftlimit"+ leftLimit.ToString());
+        Debug.Log("rightlimit"+ rightLimit.ToString());
+        Debug.Log("transform"+ transform.position.x.ToString());
+
+        if (leftLimit < transform.position.x && transform.position.x < rightLimit)
+        {
+            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        }
+        else if (transform.position.x <= leftLimit && movement > 0)
+        {
+            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        }
+        else if (transform.position.x >= rightLimit && movement < 0)
+        {
+            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        }
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
+
     }
 }
