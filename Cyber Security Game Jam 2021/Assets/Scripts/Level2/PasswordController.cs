ec2dcea69b15c38ded18c9f69655bbe0230fccf6
jovanhuang <jhtc98@hotmail.com>
2021-07-04 18:02:19 +0800

    Commented out PasswordController for Level 2.

--- Cyber Security Game Jam 2021/Assets/Scripts/Level2/PasswordController.cs
+++ Cyber Security Game Jam 2021/Assets/Scripts/Level2/PasswordController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,3 +46,4 @@ public class PasswordController : MonoBehaviour
  
 
 }
+*/
