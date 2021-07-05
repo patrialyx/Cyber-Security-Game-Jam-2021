fe51f017f1dd2a73c2388bda5807e0d88cce5e94
Patria Lim <patria.lyx@gmail.com>
2021-07-06 01:31:14 +0800

    polished menu

---
+++ Cyber Security Game Jam 2021/Assets/Scripts/1_Menu/startWholeGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class startWholeGame : MonoBehaviour
+{
+    [SerializeField] private Animator transition_anim;
+    [SerializeField] private GameObject transition_canvas;
+    [SerializeField] private string sceneName_string;
+    [SerializeField] private Rigidbody2D character_rb2D;
+    [SerializeField] private Image[] SwitchImages;
+    public void NewGame() {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene(){
+        character_rb2D.gravityScale = 3;
+        yield return new WaitForSeconds(2.5f);
+        transition_canvas.SetActive(true);
+        transition_anim.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(sceneName_string);
+    }
+}
