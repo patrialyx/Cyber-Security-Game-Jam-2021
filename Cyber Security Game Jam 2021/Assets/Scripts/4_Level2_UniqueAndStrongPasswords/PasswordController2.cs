ef41bab51c913685cac9ea296f7311338ffdd159
Patria Lim <patria.lyx@gmail.com>
2021-07-07 23:40:12 +0800

    2a_gameroompt2 works perfectly

--- Cyber Security Game Jam 2021/Assets/Scripts/4_Level2_UniqueAndStrongPasswords/PasswordController2.cs
+++ Cyber Security Game Jam 2021/Assets/Scripts/4_Level2_UniqueAndStrongPasswords/PasswordController2.cs
@@ -8,7 +8,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Linq;
 using UnityEngine.SceneManagement;
-public class PasswordController : MonoBehaviour
+public class PasswordController2 : MonoBehaviour
 {
     public static PasswordController instance;
     [SerializeField]
@@ -31,17 +31,26 @@ public class PasswordController : MonoBehaviour
     private HashSet<string> hash;
     private List<string> list;
     private string path;
+    private bool isSet;
     [SerializeField] private GameObject ladder;
     [SerializeField] private GameObject endOfPart1;
+
+    public void Start() {
+        isSet = true;
+    }
+
      public void ReadStringInput(string s)
     {
         password = s;
         errMsg.text ="";
+        Debug.Log("whats the pwd - "+password);
+        Debug.Log(password.Length.ToString());
     }
 
     public void Update()
     {
-
+        if (isSet)
+        {
             if (Input.GetKeyDown(KeyCode.Return)) 
             {
                 Regex rgx = new Regex("[^A-Za-z0-9]");
@@ -199,9 +208,111 @@ public class PasswordController : MonoBehaviour
                 }
                 inputFieldElement.text ="";
                 inputFieldElement.ActivateInputField();
-            }  
+            }
+        }
+        else //getter mode
+        {
+            Debug.Log("iSetValue in update else: "+isSet.ToString());
+            if (Input.GetKeyDown(KeyCode.Return)) 
+            {
+                path = Application.dataPath + "/passwordLog.txt";
+                if(File.Exists(path))
+                {
+                    list = File.ReadLines(path).ToList();
+                }
+                if(list.Contains(password))
+                {
+                    inputFieldElement.image.color = Color.green;
+                    errMsg.color = Color.green;
+                    errMsg.text = "That is the correct password. Successful login.";
+                    isStrongPwd = true;
+                }  
+                else
+                {
+                    inputFieldElement.image.color = Color.red;
+                    errMsg.text = "Incorrect password. Try again.";
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Return)) 
+            { 
+                inputFieldElement.image.color = Color.white;
+                if (isStrongPwd) // if password passes
+                {
+                    mainDoor.SetActive(true);
+                    inputField.SetActive(false);
+                    switch (machineNumber)
+                    {
+                        case 1:
+                            door[0].SetActive(true);
+                            // shieldAnim[0].SetBool(boolstr, true);
+                            shield[9].SetActive(true);
+                            shield[0].SetActive(false);
+                            break;
+                        case 2:
+                            door[1].SetActive(true);
+                            // shieldAnim[1].SetBool(boolstr+"1", true);
+                            shield[0].SetActive(true);
+                            shield[1].SetActive(false);
+                            break;
+                        case 3:
+                            door[2].SetActive(true);
+                            // shieldAnim[2].SetBool(boolstr+"2", true);
+                            shield[1].SetActive(true);
+                            shield[2].SetActive(false);
+                            break;
+                        case 4:
+                            door[3].SetActive(true);
+                            // shieldAnim[3].SetBool(boolstr+"3", true);
+                            shield[2].SetActive(true);
+                            shield[3].SetActive(false);
+                            break;
+                        case 5:
+                            door[4].SetActive(true);
+                            gateAnim[0].SetBool("playMovingUp", true);
+                            // shieldAnim[4].SetBool(boolstr+"4", true);
+                            shield[3].SetActive(true);
+                            shield[4].SetActive(false);
+                            break;
+                        case 6:
+                            door[5].SetActive(true);
+                            // shieldAnim[5].SetBool(boolstr+"5", true);
+                            shield[4].SetActive(true);
+                            shield[5].SetActive(false);
+                            break;
+                        case 7:
+                            door[6].SetActive(true);
+                            // shieldAnim[6].SetBool(boolstr+"6", true);
+                            shield[5].SetActive(true);
+                            shield[6].SetActive(false);
+                            break;
+                        case 8:
+                            door[7].SetActive(true);
+                            // shieldAnim[7].SetBool(boolstr+"7", true);
+                            shield[6].SetActive(true);
+                            shield[7].SetActive(false);
+                            break;
+                        case 9:
+                            door[8].SetActive(true);
+                            // shieldAnim[8].SetBool(boolstr+"8", true);
+                            shield[7].SetActive(true);
+                            shield[8].SetActive(false);
+                            break;
+                        case 10:
+                            gateAnim[1].SetBool("isPressed", true);
+                            door[9].SetActive(true);
+                            shield[8].SetActive(true);
+                            shield[9].SetActive(false);
+                            break;
+                    }
+                }
+                inputFieldElement.text ="";
+                inputFieldElement.ActivateInputField();
+            }
+        }
+       
     }
     void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("ontrigger isOnSet value:"+isSet.ToString());
       
         if (other.gameObject.name == "TileGroundTop (62)")
         {
@@ -210,8 +321,20 @@ public class PasswordController : MonoBehaviour
         if (other.gameObject.name == "button (11)")
         {
             pwdInputField.SetActive(false);
-            gamePart2_dialogue.SetActive(true);
-            endOfPart1.SetActive(true);
+            if(isSet)
+            {
+                placeholder.text = "Login using previously set password...";
+                gamePart2_dialogue.SetActive(true);
+                endOfPart1.SetActive(true);
+            }
+            else
+            {
+                pwdInputField.SetActive(false);
+                endOfPart1.SetActive(false);
+                gamePart2_dialogue.SetActive(true);
+                endGameWin.SetActive(true);
+                ladder.SetActive(true);
+            }
         }
         else
         {
@@ -267,6 +390,7 @@ public class PasswordController : MonoBehaviour
                 afterActionSettings();
             }
         }
+        
     }
     private void afterActionSettings()
     {
