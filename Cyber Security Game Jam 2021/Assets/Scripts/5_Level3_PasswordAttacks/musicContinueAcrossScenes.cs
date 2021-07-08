280355fb5a9340c1a5592be8ca2adb207ff66cbf
Patria Lim <patria.lyx@gmail.com>
2021-07-08 14:30:01 +0800

    done with changes that i might wanna discard

---
+++ Cyber Security Game Jam 2021/Assets/Scripts/5_Level3_PasswordAttacks/musicContinueAcrossScenes.cs
@@ -0,0 +1,22 @@
+﻿ using UnityEngine;
+ 
+ public class musicContinueAcrossScenes : MonoBehaviour
+ {
+     [SerializeField]
+     private AudioSource _audioSource;
+     private void Awake()
+     {
+         DontDestroyOnLoad(transform.gameObject);
+     }
+ 
+     public void PlayMusic()
+     {
+         if (_audioSource.isPlaying) return;
+         _audioSource.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         _audioSource.Stop();
+     }
+ }
